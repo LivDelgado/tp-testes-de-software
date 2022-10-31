525deb91ec0889e76aaea759fae10036721adbf0
Lívia Delgado <23265664+LivDelgado@users.noreply.github.com>
2022-10-31 22:49:31 +0000

    inicia implementação de testes do lista_service

---
+++ tests/lista/test_lista_service.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+from app.lista.lista_compras import ListaCompras
+from app.lista.lista_service import ListaService
+from app.usuario.usuario import Usuario
+
+
+class TestListaService(TestCase):
+    def setUp(self) -> None:
+        self.usuario = Usuario()
+        self.lista_service = ListaService()
+        return super().setUp()
+
+    def test_obter_listas_WHEN_nenhuma_lista_cadastrada_THEN_raise_error(self):
+        with self.assertRaises(ValueError):
+            self.lista_service.obter_listas(self.usuario)
+
+    def test_obter_listas_WHEN_lista_cadastrada_THEN_retorna_listas(self):
+        self.usuario.adicionar_lista(ListaCompras(nome="teste", descricao="teste"))
+
+        listas = self.lista_service.obter_listas(self.usuario)
+
+        self.assertEqual(1, len(listas))
+
